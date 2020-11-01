ac6a6c12b243406be61b893cf7b707fd0df31827
hemanth <hemanthmurukuri@icloud.com>
2020-11-01 16:41:16 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,43 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+import { Route,BrowserRouter as Router } from 'react-router-dom';
+import loginComponent from './login/login';
+import signupComponent from './signup/signup';
+import dashboardComponent from './dashboard/dashboard';
+import firebase from 'firebase/app';
+
+
+require("firebase/firestore");
+firebase.initializeApp({
+  apiKey: "AIzaSyCCnxItOzY_fBxW1VDiRFd3r5XdBLlRKfM",
+  authDomain: "chatapp-7bddb.firebaseapp.com",
+  databaseURL: "https://chatapp-7bddb.firebaseio.com",
+  projectId: "chatapp-7bddb",
+  storageBucket: "chatapp-7bddb.appspot.com",
+  messagingSenderId: "1008472640668",
+  appId: "1:1008472640668:web:9a0c5e32a4548778d23159",
+  measurementId: "G-9GHRRTJ41X"
+})
+const routing=(
+  <Router>
+    <div id="routing-container">
+      <Route path='/login' component={loginComponent}>
+
+      </Route>
+      <Route path="/signup" component={signupComponent}></Route>
+      <Route path="/dashboard" component={dashboardComponent}></Route>
+    </div>
+  </Router>
+)
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+
+    routing
+ ,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+
